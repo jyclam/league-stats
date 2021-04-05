19ad80a78caec8e91c45aa8de56a0aa7dc788352
John <john.dev.work.01@gmail.com>
2021-04-05 14:43:24 -0700

    Add culled static data, serving from server

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use("/static", express.static("public"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
