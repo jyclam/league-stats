d20be479a31fa53e15ff5233a8c980c9bdd9c768
John <john.dev.work.01@gmail.com>
2021-04-05 21:08:59 -0700

    Configure url for production

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,5 +1,6 @@
+const PRODUCTION_DOMAIN = process.env.PRODUCTION_DOMAIN || "50.18.167.254";
 const DOMAIN =
-  process.env.NODE_ENV === "development" ? "localhost" : "54.183.157.168";
+  process.env.NODE_ENV === "development" ? "localhost" : PRODUCTION_DOMAIN;
 
 export const STATIC_ASSETS_URL = `http://${DOMAIN}:5000/static`;
 export const API_ENDPOINT_URL = `http://${DOMAIN}:5000/api/v1/summoner`;
